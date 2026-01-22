fa6c47d1c3a5edddd453a70902aefff478af9d42
Ian Gow <iandgow@gmail.com>
2026-01-22 10:14:14 -0500

    Reformatted code.
     - Removed useless comments (pre-modifed code).

--- code/fdic_pull.py
+++ code/fdic_pull.py
@@ -3,23 +3,8 @@ import pandas as pd
 import requests
 import json
 
-
-
 # Get Institutions
 banks_id = pd.read_csv("../data/input/fdic/cert_nums.csv")
-
-
-# url= "https://banks.data.fdic.gov/api/institutions"
-
-# params = dict(
-#     search    = "NAME: Bank of Hawaii",
-#     fields    = "NAME",
-#     limit      = 10000,
-#     )
-# resp = requests.get(url=url, params = params)
-# data = resp.json()
-# data
-# [data["data"][0]["data"][x] for x in ["NAME","CERT"]]
 
 url= "https://banks.data.fdic.gov/api/financials"
 output = [["CERT", "DEP", "DEPINS", "ASSETS"]]
@@ -40,11 +25,15 @@ for bank in banks_id['fdic_cert_num']:
     resp = requests.get(url=url, params = params)
     data = resp.json()
     try:
-        output.append([data["data"][0]["data"][x] for x in ["CERT","DEP","DEPINS", "ASSET"]])
+        output.append([data["data"][0]["data"][x] 
+                          for x in ["CERT","DEP","DEPINS", "ASSET"]])
     except:
         pass
 
-bank_data_final = pd.DataFrame(output[1:], columns = ["CERT", "DEP", "DEPINS", "ASSETS"]).merge(right=banks_id, left_on = "CERT", right_on = "fdic_cert_num")
+bank_data_final = (pd.DataFrame(output[1:], 
+                                columns = ["CERT", "DEP", "DEPINS", "ASSETS"])
+                   .merge(right=banks_id, 
+                         left_on = "CERT", right_on = "fdic_cert_num")
 
 bank_data_final.to_csv("../data/input/fdic/bank_deposits.csv")
 
